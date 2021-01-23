a33fc2df9101cadd7c6430a80df858af2b1133a8
Selami Tunca <selami.tunca33@gmail.com>
2021-01-24 02:19:57 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course1 = new Course();
+            course1.CourseName = "Kobra";
+            course1.CourseName = "Kobra";
+            course1.watchRatio = 32;
+
+            Course course2 = new Course();
+            course2.CourseName = "Kobra";
+            course2.CourseName = "Kobra";
+            course2.watchRatio = 32;
+
+
+            Course[] courses = new Course[] { course1, course2 };
+
+            foreach (Course course in courses)
+            {
+                Console.WriteLine("Course Name: " + course.CourseName);
+            }
+
+
+        
+        }
+ }
+
+        class Course
+        {
+
+    public string CourseName { get; set; }
+    public string educator { get; set; }
+    public int watchRatio { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string course1 = "A";
+            string course2 = "B";
+
+            string[] courses = new string[] {"A","B","C" }; 
+            
+
+            for (int i = 0; i <=courses.Length; i++)
+            {
+
+                Console.WriteLine(courses[i]);
+
+
+            }
+            Console.WriteLine("******************");
+
+   
+
+            foreach (string course in courses)
+            {
+                Console.WriteLine(course);
+
+            }
+
+
+           
+
+
+
+        }
+    }
+}
